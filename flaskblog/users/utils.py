235272516ced211382d3e7a97ca0c10edc2f9ef6
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-06-18 21:54:00 +0600

    Completed the flaskblog project

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,25 @@
+import os
+import secrets
+from PIL import Image
+from flask import render_template, current_app
+from flask_mail import Message
+from flaskblog import mail
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+    return picture_fn
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender='sabbir.ahmed845@gmail.com', recipients=[user.email])
+    msg.html = render_template('reset_password.html', user=user, token=token)
+    mail.send(msg)
