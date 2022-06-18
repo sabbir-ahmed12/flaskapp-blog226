39c7e36fe55681f32ffff727a03e645c04637b1d
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-06-18 23:39:54 +0600

    Updated init.py file of flaskblog.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -20,8 +20,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    with app.app_context():
-        db.create_all()
+    db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
