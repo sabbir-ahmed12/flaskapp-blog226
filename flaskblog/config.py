b8d360dd649e6eab094617bbb64ac09ea9b09493
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-07-09 20:10:13 +0600

    Replace postgres in database url with postgresql.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,7 @@ class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
